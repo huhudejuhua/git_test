5ece141b81cb6b065fe61c48da635e6f76791d35
zyh <416004900@qq.com>
2020-06-01 15:51:25 +0800

    本地工程添加到git版本库

---
+++ gittest01/src/com/itheima/git/firstclass.java
@@ -0,0 +1,8 @@
+package com.itheima.git;
+
+public class firstclass {
+    public static void main(String[] args) {
+        System.out.println("git 01");
+        System.out.println("git 01");
+    }
+}
